b0c32175fb25a4dfcaf713887bed9586101811f3
chizzykas <chizzykas@gmail.com>
2023-07-25 03:02:37 +0100

    Random WordPair Gen from codelabs experimented on

--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,259 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:new_flutter_test/second_page.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: const MyApp(),
-    routes: {
-      '/home': (context) => const MyApp(),
-      '/nextPage': (context) => const NextPage(),
-    },
-    theme: ThemeData.from(
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.green)),
-  ));
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        routes: {
+          '/': (context) => MyHomePage(),
+          '/second': (context) => NextPage(),
+          '/third': (context) => Placeholder(),
+        },
+        //Define the theme of the app
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
+          floatingActionButtonTheme: FloatingActionButtonThemeData(
+            foregroundColor: Theme.of(context).colorScheme.onSecondary,
+            backgroundColor: Theme.of(context).colorScheme.secondary,
+          ),
+        ),
+        // home: MyHomePage(),
+      ),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  int counter = 0;
+//Create class to get random word pairs
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_outline;
+    }
+
     return Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            ListTile(
-              title: const Text('Tile 1'),
-              onTap: () {
-                print('Tap recieved');
-                Navigator.pushNamed(context, '/nextPage');
-                Navigator.pop(context);
-              },
-            ),
-            const Divider(),
-            ListTile(
-              title: const Text('Tile 2'),
-              onTap: () {
-                Navigator.pushNamed(context, '/home');
-                Navigator.pop(context);
-              },
-            ),
-          ],
+      backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+      drawer: newDrawer(context),
+      appBar: newAppBar(context, "Home Page"),
+      body: GeneratorPage(pair: pair, appState: appState, icon: icon),
+      floatingActionButton: newFAButton(
+        context,
+        Icon(
+          Icons.favorite,
         ),
+        "second",
       ),
-      appBar: AppBar(
-        title: const Text("A New Flutter Test"),
-        elevation: 0.0,
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "This is a counter:",
-              style: TextStyle(
-                fontSize: 20.0,
-              ),
+    );
+  }
+
+  Drawer newDrawer(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+            // margin: EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 3.0),
+            padding: EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 3.0),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.secondary,
             ),
-            const SizedBox(height: 10.0),
-            Text(
-              "$counter",
-              style: const TextStyle(
-                fontSize: 30.0,
-              ),
-            ),
-            const SizedBox(height: 15.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            child: Row(
               children: [
-                TextButton.icon(
-                  onPressed: () {
-                    if (counter > 0) {
-                      setState(() {
-                        counter--;
-                      });
-                    } else {
-                      counter = 0;
-                    }
-                  },
-                  icon: const Icon(Icons.remove),
-                  label: const Text("Decrease Counter"),
-                  style: TextButton.styleFrom(
-                      // backgroundColor: ColorScheme.fromSwatch().primary,
-                      ),
+                CircleAvatar(),
+                SizedBox(
+                  width: 10.0,
                 ),
-                ElevatedButton.icon(
-                    onPressed: () {
-                      setState(() {
-                        counter++;
-                      });
-                    },
-                    icon: const Icon(Icons.add),
-                    label: const Text("Increase Counter")),
+                Text(
+                  "The Drawer Header",
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.onSecondary,
+                  ),
+                ),
               ],
             ),
-            OutlinedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/nextPage');
-              },
-              child: const Text('Next Page >'),
+          ),
+          ListTile(
+            title: Row(
+              children: [
+                Icon(Icons.home),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text("Home"),
+              ],
             ),
-          ],
+            onTap: () => Navigator.pushNamed(context, '/'),
+          ),
+          ListTile(
+            title: Row(
+              children: [
+                Icon(Icons.favorite),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text("Favourites"),
+              ],
+            ),
+            onTap: () => Navigator.pushNamed(context, '/second'),
+          ),
+          ListTile(
+            title: Row(
+              children: [
+                Icon(Icons.circle),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text("Empty Page"),
+              ],
+            ),
+            onTap: () => Navigator.pushNamed(context, '/third'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  FloatingActionButton newFAButton(
+      BuildContext context, Icon icon, String route) {
+    return FloatingActionButton(
+      onPressed: (() {
+        Navigator.pushNamed(context, '/$route');
+      }),
+      backgroundColor: Theme.of(context).colorScheme.secondary,
+      child: icon,
+    );
+  }
+
+  AppBar newAppBar(BuildContext context, String title) {
+    return AppBar(
+      iconTheme: IconThemeData(
+        color: Theme.of(context).colorScheme.onSecondary,
+      ),
+      backgroundColor: Theme.of(context).colorScheme.secondary,
+      title: Text(
+        title,
+        style: TextStyle(
+          color: Theme.of(context).colorScheme.onSecondary,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            counter = 0;
-          });
-        },
-        child: const Icon(Icons.refresh),
-        tooltip: "Reset the counter",
+      centerTitle: true,
+      elevation: 10.0,
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
+  const GeneratorPage({
+    super.key,
+    required this.pair,
+    required this.appState,
+    required this.icon,
+  });
+
+  final WordPair pair;
+  final MyAppState appState;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('A random idea:'),
+          BigCard(pair: pair),
+          SizedBox(
+            height: 10.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(
+                width: 10.0,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    Key? key,
+    required this.pair,
+  }) : super(key: key);
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final wordStyle = theme.textTheme.displayLarge!.copyWith(
+      color: theme.colorScheme.onSecondary,
+    );
+
+    return Card(
+      color: theme.colorScheme.secondary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          pair.asPascalCase,
+          style: wordStyle,
+          semanticsLabel: pair.asPascalCase,
+        ),
       ),
     );
   }
--- lib/second_page.dart
+++ lib/second_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:new_flutter_test/main.dart';
+import 'package:provider/provider.dart';
 
 class NextPage extends StatefulWidget {
   const NextPage({super.key});
@@ -11,20 +12,65 @@ class NextPage extends StatefulWidget {
 }
 
 class _NextPageState extends State<NextPage> {
+  MyHomePage homePageObj = MyHomePage();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Second Page'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/home');
-          },
-          child: Text('Go to Home Page'),
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Scaffold(
+        drawer: homePageObj.newDrawer(context),
+        appBar: homePageObj.newAppBar(context, "Favourites Page"),
+        body: Container(
+          color: Theme.of(context).colorScheme.primaryContainer,
+          child: Center(
+            child: Text(
+              'No Favourites yet',
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Theme.of(context).colorScheme.onPrimaryContainer,
+              ),
+            ),
+          ),
         ),
-      ),
-    );
+        floatingActionButton: homePageObj.newFAButton(
+          context,
+          Icon(Icons.home),
+          '',
+        ),
+      );
+    } else {
+      return Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+        drawer: homePageObj.newDrawer(context),
+        appBar: homePageObj.newAppBar(context, "Favourites Page"),
+        body: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Text('You have '
+                  '${appState.favorites.length} favourites:'),
+            ),
+            for (var pair in appState.favorites)
+              ListTile(
+                leading: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      appState.favorites.remove(pair);
+                    });
+                  },
+                  icon: Icon(Icons.delete),
+                ),
+                title: Text(pair.asPascalCase),
+              ),
+          ],
+        ),
+        floatingActionButton: homePageObj.newFAButton(
+          context,
+          Icon(Icons.home),
+          '',
+        ),
+      );
+    }
   }
 }
